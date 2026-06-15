4a815cef90b165a44172128efbe840fa9b643e08
fmfarukhossen702 <fmfarukhossen702@gmail.com>
2026-06-16 01:25:16 +0600

    frist commit

---
+++ script.js
@@ -0,0 +1,75 @@
+
+
+
+let counter = document.querySelectorAll('.counter');
+let arr = Array.from(counter)
+   
+function handleClick () { arr.forEach((data)=>{
+    let count = 0    
+    console.log(data.dataset.number);
+
+  let stopp = (()=>{ 
+
+     count++
+    data.innerHTML = count;    
+   
+    if (count == data.dataset.number){
+        clearInterval(stopInterval);
+    }})
+    let stopInterval = setInterval(() => {
+        stopp()
+    },data.dataset.number/100)
+   })
+}
+
+let message = document.getElementById('message')
+let count = 10;
+
+let time = setInterval(()=>{
+    message.classList.add('pop')
+    setTimeout(() => {
+                message.classList.remove('pop')
+
+    }, 300);
+
+    message.innerHTML = count;
+    count--;
+    if (count < 0){
+        clearInterval(time)
+        message.innerHTML = "FIFA WORLD CUP 2026"
+        message.classList.add("text-black");
+        message.style.backgroundImage = "url('/image/img.jpg')";
+        message.style.backgroundSize = "cover";
+        message.style.backgroundPosition = "center";
+        message.style.backgroundRepeat = "no-repeat"
+    }
+},1000)
+
+
+
+let messag = document.getElementById("sevenUp");
+
+let counte = 10;
+
+let timee = setInterval(() => {
+  messag.classList.add("pop");
+  setTimeout(() => {
+    messag.classList.remove("pop");
+  }, 300);
+
+  messag.innerHTML = counte;
+  counte--;
+  if (counte < 0) {
+    clearInterval(timee);
+    messag.innerHTML = "";
+    // messag.classList.add("text-black");
+    messag.style.backgroundImage = "url('/image/brazilSevenUp.png')";
+    messag.style.backgroundSize = "cover";
+    messag.style.backgroundPosition = "center";
+    messag.style.backgroundRepeat = "no-repeat";
+  }
+}, 1000);
+
+
+
+
